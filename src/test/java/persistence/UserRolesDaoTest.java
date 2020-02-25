e4f28b2afe4d300d11a4c7cc29a672cf2ec99b86
erbecerra <estefaniereyna@gmail.com>
2020-02-25 00:20:56 -0600

    Set up one to many relationship from user to user roles

--- src/test/java/persistence/UserRolesDaoTest.java
+++ src/test/java/persistence/UserRolesDaoTest.java
@@ -1,4 +1,77 @@
 package persistence;
 
+import entity.User;
+import entity.UserRoles;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UserRolesDaoTest {
+
+    UserRolesDao dao;
+
+    /**
+     * Run set up tasks before each test:
+     * 1. execute sql which deletes everything from the table and inserts records)
+     * 2. Create any objects needed in the tests
+     */
+    @BeforeEach
+    void setUp() {
+
+        util.Database database = util.Database.getInstance();
+       // database.runSQL("cleandb.sql");
+
+        dao = new UserRolesDao();
+    }
+
+    /**
+     * Verify successful retrieval of a user
+     */
+    @Test
+    void getByIdSuccess() {
+        User retrievedUser = dao.getById(3);
+        assertEquals("Barney", retrievedUser.getFirstName());
+        assertEquals("Curry", retrievedUser.getLastName());
+        assertEquals("bcurry", retrievedUser.getUserName());
+    }
+
+    /**
+     * Verify successful insert of a user
+     */
+    @Test
+    void insertSuccess() {
+
+        User newUser = new User("Fred", "Flintstone", "fflintstone", LocalDate.parse("1968-01-01"));
+        int id = dao.insert(newUser);
+        assertNotEquals(0,id);
+        User insertedUser = dao.getById(id);
+        assertEquals("Fred", insertedUser.getFirstName());
+        // Could continue comparing all values, but
+        // it may make sense to use .equals()
+        // TODO review .equals recommendations http://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#mapping-model-pojo-equalshashcode
+    }
+
+    /**
+     * Verify successful delete of user
+     */
+    @Test
+    void deleteSuccess() {
+        dao.delete(dao.getById(3));
+        assertNull(dao.getById(3));
+    }
+
+    /**
+     * Verify successful retrieval of all users
+     */
+    @Test
+    void getAllSuccess() {
+        List<UserRoles> roles = dao.getAll();
+        assertEquals(6, roles.size());
+    }
+
 }
